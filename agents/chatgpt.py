a4c62e12670f986035b80a0ebfcb95b871975820
Krishna Dwivedi <150330334+SudoHopeX@users.noreply.github.com>
2026-02-24 09:17:48 +0530

    Update chatgpt.py

--- agents/chatgpt.py
+++ agents/chatgpt.py
@@ -2,14 +2,14 @@
 
 # /agents/chatgpt.py
 # KaliGPT ChatGPT_agent
-# Updated: 2 feb 2026
+# Updated: 24 feb 2026
 
 
 from openai import OpenAI
 import sys
 
 from .utils.parse_n_print_response import parse_n_print_response
-from .utils.prompts import SYSTEM_PROMPT
+from .utils.prompts import WEB_BUG_BOUNTY_AGENT as SYSTEM_PROMPT
 from .utils.agent_configs import get_api_key, get_default_model, get_ai_specific_default_model
 from .utils.tools import get_tools_info
 from .utils.agent_management import agent_management, AI_MANAGEMENT_OPTIONS
